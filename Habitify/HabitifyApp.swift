8478a0ed6010f4de69a04a042c7c912056558a1d
Vedant Ambre <ambreved3@gmail.com>
2026-04-08 12:55:40 +0100

    Initial Commit

---
+++ Habitify/HabitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitifyApp.swift
+//  Habitify
+//
+//  Created by Vedant Ambre on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
